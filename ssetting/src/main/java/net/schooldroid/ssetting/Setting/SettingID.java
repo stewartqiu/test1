26f6a684342e02f7abed9cf081b0ed48c77ce4ac
stewartqiu <33251362+stewartqiu@users.noreply.github.com>
2017-11-01 13:30:10 +0700

    new Setting

---
+++ ssetting/src/main/java/net/schooldroid/ssetting/Setting/SettingID.java
@@ -0,0 +1,19 @@
+package net.schooldroid.ssetting.Setting;
+
+/**
+ * Created by Jekky on 21/10/2017.
+ */
+
+public class SettingID {
+
+    public static String npsn = "NPSN";
+    public static String namaSekolah = "NamaSekolah";
+    public static String negara = "Negara";
+    public static String provinsi = "Provinsi";
+    public static String kota = "Kota";
+
+    public static String radius = "Radius";
+    public static String latPatokan ="LatitudePatokan";
+    public static String longPatokan ="LongitudePatokan";
+
+}
